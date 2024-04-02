897ca0c33f8b1c33f3606f7ea521c343be06d6a2
AdityaIbnu13 <inuadyt@gmail.com>
2024-04-02 10:22:38 +0700

    update penjualan/index.php

---
+++ modul/penjualan/index.php
@@ -0,0 +1,23 @@
+<div class="card mb-3">
+    <div class="card-body">
+        <form action="" method="post">
+            <div class="row mb-3">
+                <div class="col-md-4">
+                    <label for="invoice" class="form-label">Invoice</label>
+                    <input type="text" class="form-control" name="invoice">
+                </div>
+                <div class="col-md-4">
+                    <label for="tanggal" class="form-label">Tanggal</label>
+                    <input type="date" class="form-control" name="tanggal">
+                </div>
+                <div class="col-md-4">
+                    <label for="barang" class="form-label">Barang</label>
+                    <select name="barang" class="form-select">
+                        <option value="1">Laptop Acer</option>
+                        <option value="2">Komputer (PC)</option>
+                    </select>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
