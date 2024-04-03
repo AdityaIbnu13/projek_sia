81c78b6f814796fb2262bb8874376ce24e29eb05
AdityaIbnu13 <inuadyt@gmail.com>
2024-04-03 09:01:43 +0700

    update barang/index.pho

---
+++ modul/barang/index.php
@@ -0,0 +1,33 @@
+<div class="card mb-3">
+    <div class="card-body">
+        <form action="" method="post">
+            <div class="row">
+                <div class="mb-3 col-md-6">
+                    <label for="nama_barang" class="form-label">Nama barang</label>
+                    <input type="text" class="form-control" name="nama_barang">
+                </div>
+                <div class="mb-3 col-md-6">
+                    <label for="harga_beli" class="form-label">Harga beli</label>
+                    <input type="number" class="form-control" name="harga_beli">
+                </div>
+            </div>
+            <div class="row">
+                <div class="mb-3 col-md-6">
+                    <label for="harga_jual" class="form-label">Harga jual</label>
+                    <input type="number" class="form-control" name="harga_jual">
+                </div>
+                <div class="mb-3 col-md-6">
+                    <label for="stok" class="form-label">Stok</label>
+                    <input type="number" class="form-control" name="stok">
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col text-end">
+                    <button class="btn btn-secondary" type="reset">Reset</button>
+                    <button class="btn btn-primary" type="submit">Simpan</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
