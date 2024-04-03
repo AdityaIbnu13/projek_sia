8dc7a987262ee3b0245e14c95efe46f5ff1c1385
AdityaIbnu13 <inuadyt@gmail.com>
2024-04-03 09:09:10 +0700

    update-3 barang/index.php

--- modul/barang/index.php
+++ modul/barang/index.php
@@ -31,3 +31,56 @@
         </form>
     </div>
 </div>
+<div class="card">
+    <div class="card-header">
+        <h3>Data Barang</h3>
+    </div>
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Barang</th>
+                        <th>Harga beli</th>
+                        <th>Harga jual</th>
+                        <th>Stok</th>
+                        <th><i class="bi bi-gear-fill"></i></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Laptop ASUS</td>
+                        <td>Rp. 10.000.000</td>
+                        <td>Rp. 12.000.000</td>
+                        <td>5</td>
+                        <td>
+                            <a href="#editBarang" class="text-decoration-none" data-bs-toggle="modal">
+                                <i class="bi bi-pencil-square text-success"></i>
+                            </a>
+                            <a href="" class="text-decoration-none">
+                                <i class="bi bi-trash text-danger"></i>
+                            </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Monitor Samsung</td>
+                        <td>Rp. 5.000.000</td>
+                        <td>Rp. 6.000.000</td>
+                        <td>10</td>
+                        <td>
+                            <a href="#editBarang" class="text-decoration-none" data-bs-toggle="modal">
+                                <i class="bi bi-pencil-square text-success"></i>
+                            </a>
+                            <a href="" class="text-decoration-none">
+                                <i class="bi bi-trash text-danger"></i>
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
