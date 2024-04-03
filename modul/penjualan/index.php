da49b414a6cd3837e23ef355770a2316d9061b69
AdityaIbnu13 <inuadyt@gmail.com>
2024-04-03 08:49:32 +0700

    update penjualan/index.php

--- modul/penjualan/index.php
+++ modul/penjualan/index.php
@@ -18,6 +18,44 @@
                     </select>
                 </div>
             </div>
+            <div class="row mb-3">
+                <div class="col-md-4">
+                    <label for="pelanggan" class="form-label">Pelanggan</label>
+                    <select name="pelanggan" class="form-select">
+                        <option value="1">PT Sejahtera</option>
+                        <option value="2">CV Maju Bersama</option>
+                    </select>
+                </div>
+                <div class="col-md-2">
+                    <label for="jumlah" class="form-label">Jumlah</label>
+                    <input type="number" class="form-control" name="jumlah">
+                </div>
+                <div class="col-md-3">
+                    <label for="harga" class="form-label">Harga</label>
+                    <div class="input-group">
+                        <span class="input-group-text">Rp.</span>
+                        <input type="number" class="form-control" name="harga">
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <label for="total" class="form-label">Total</label>
+                    <div class="input-group">
+                        <span class="input-group-text">Rp.</span>
+                        <input type="number" class="form-control" name="total" disabled>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <label for="keterangan" class="form-label">Keterangan</label>
+                    <textarea name="keterangan" class="form-control"></textarea>
+                </div>
+            </div>
+            <hr class="text-secondary">
+            <div class="text-end">
+                <button type="reset" class="btn btn-secondary">Reset</button>
+                <button type="submit" class="btn btn-primary">Simpan</button>
+            </div>
         </form>
     </div>
 </div>
