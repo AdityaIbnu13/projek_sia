7d3a1a8902c08e4091d8488fc8cb24f4e751c8d7
AdityaIbnu13 <inuadyt@gmail.com>
2024-04-03 09:16:42 +0700

    update-3 barang/index.php

--- modul/barang/index.php
+++ modul/barang/index.php
@@ -84,3 +84,36 @@
         </div>
     </div>
 </div>
+<!-- Modal -->
+<div class="modal fade" id="editBarang" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Edit Barang</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="mb-3">
+                    <label class="form-label" for="nama_barang">Barang</label>
+                    <input type="text" class="form-control" name="nama_barang" value="Laptop ASUS">
+                </div>
+                <div class="mb-3">
+                    <label class="form-label" for="harga_beli">Harga beli</label>
+                    <input type="number" class="form-control" name="harga_beli" value="10000000">
+                </div>
+                <div class="mb-3">
+                    <label class="form-label" for="harga_jual">Harga jual</label>
+                    <input type="number" class="form-control" name="harga_jual" value="12000000">
+                </div>
+                <div class="mb-3">
+                    <label class="form-label" for="stok">Stok</label>
+                    <input type="number" class="form-control" name="stok" value="5">
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Simpan</button>
+            </div>
+        </div>
+    </div>
+</div>
